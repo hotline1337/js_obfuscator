19ec991eda0cf4b7a2e3a59ec519504941646b42
hotline1337 <32012110+hotline1337@users.noreply.github.com>
2021-06-20 16:00:30 +0200

    Add files via upload

---
+++ js_obfuscator/Auth.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using VMProtect;
+
+namespace js_obfuscator
+{
+    class Auth
+    {
+        private const string version = "1.8";
+
+        public static void authorize()
+        {
+            WebClient httpClient = new WebClient(); WebRequest.DefaultWebProxy = new WebProxy();
+            bool _checked = false;
+            string hwid = SDK.GetCurrentHWID();
+
+            Console.WriteLine("[*] Authorizing client: " + hwid);
+
+            httpClient.Headers.Add("Hotline-Services", "list_access");
+            if (httpClient.DownloadString("https://badtools.000webhostapp.com/obf/list.txt").Contains(hwid))
+            {
+                _checked = true;
+            }
+
+            if (!_checked)
+            {
+                Console.WriteLine("[*] Failed to authorize");
+                throw new Exception("Authorization failed");
+            }
+            Console.WriteLine("[*] Authorized successfully");
+        }
+
+        public static void check_version()
+        {
+            WebClient httpClient = new WebClient(); WebRequest.DefaultWebProxy = new WebProxy();
+
+            httpClient.Headers.Add("Hotline-Services", "list_access");
+            if (httpClient.DownloadString("https://badtools.000webhostapp.com/obf/ver.txt") == version)
+            {
+                Console.Title = $"JS Obfuscator by hotline#1337 | {version}";
+                Console.WriteLine("[*] Using newest obfuscator");
+            }
+            else
+            {
+                Console.WriteLine("[*] Obfuscator is outdated");
+                throw new Exception("Outdated obfuscator");
+            }
+
+        }
+    }
+}
---
+++ js_obfuscator/Encode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace js_obfuscator
+{
+    class Encode
+    {
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+        public static string ToHexString(string str)
+        {
+            var sb = new StringBuilder();
+            var encoding = Encoding.GetEncoding(1256);
+            var bytes = encoding.GetBytes(str);
+            foreach (var t in bytes)
+            {
+                sb.AppendFormat("\\x{0:X2}", t);
+            }
+            return sb.ToString();
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
